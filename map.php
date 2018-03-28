a101d41eb2ddc8592d5d6fbbdb95bb05b41058b8
nilampuspitaa <dyah1431062@sttpln.ac.id>
2018-03-28 10:09:27 +0700

    oi

--- map.php
+++ map.php
@@ -1,3 +1,5 @@
+
+
 <!DOCTYPE html> 
 <html lang=en>
   <head>
@@ -10,28 +12,39 @@
 
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-    <title> Google Maps Demo </title>
+    <title> Google Maps Gardu Induk </title>
     
     </head>
   <body>
-   <h3> Google Maps Demo</h3>
+   <h3> Google Maps Gardu Induk</h3>
     <div id=map style="width:100%;height:500px"></div>
-    <script>
-      function initMap() {
+<?php
+    $conn = new mysqli('localhost', 'root', '', 'mapspace');
 
-        var uluru = {lat: 20.6, lng: 10.5};
-        var map = new google.maps.Map(document.getElementById('map'), {
-          zoom: 4,
-          center: uluru
-        });
-        var marker = new google.maps.Marker({
-          position: uluru,
-          map: map
-        });
+    $sql = mysqli_query($conn,"select * from gardu_induk");
+      if(mysqli_num_rows($sql)>=0){
+        $no = 1;
+        while($data = mysqli_fetch_array($sql)){
+          echo "<script>
+          function initMap() {
 
-      }
+            var $data[GI_NAMA] = {lat: $data[GI_KOORX], lng: $data[GI_KOORY]};
+            var map = new google.maps.Map(document.getElementById('map'), {
+              zoom: 4,
+              center: $data[GI_NAMA] 
+            });
+            var marker = new google.maps.Marker({
+              position: $data[GI_NAMA] ,
+              map: map
+            });
 
-    </script>
+          }
+
+          </script>";
+        $no++;}}
+
+    
+    ?>
     <script async defer
     src="http://maps.googleapis.com/maps/api/js?key=AIzaSyCnYMYY8GtuxqOPL_1-KjEUQU91L-0J0-s&callback=initMap">
     </script>
